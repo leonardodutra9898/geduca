cd077545115ee65ee8b5116b61e6423ff7ea561d
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-08-08 16:52:44 -0300

    #1 - inicializando projeto

---
+++ src/main/java/org/idea/geduca/gedu/GeduApplication.java
@@ -0,0 +1,12 @@
+package org.idea.geduca.gedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeduApplication.class, args);
+	}
+}
