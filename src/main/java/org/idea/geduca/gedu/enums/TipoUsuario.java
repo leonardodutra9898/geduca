90295a2255903098265540d2b579b964e0233fac
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-08-09 13:11:29 -0300

    #2 - Adicionando Model e Repositório

---
+++ src/main/java/org/idea/geduca/gedu/enums/TipoUsuario.java
@@ -0,0 +1,5 @@
+package org.idea.geduca.gedu.enums;
+
+public enum TipoUsuario {
+	ADMINISTRADOR, DIRETOR, TECNICO, AUXILIAR, SUPERVISOR, SECRETARIO
+}
---
+++ src/main/java/org/idea/geduca/gedu/model/Usuario.java
@@ -0,0 +1,59 @@
+package org.idea.geduca.gedu.model;
+
+import java.util.Date;
+
+import org.idea.geduca.gedu.enums.TipoUsuario;
+
+public class Usuario {
+
+	private long id;
+	private String nome;
+	private Date dataNasc;
+	private TipoUsuario tipoUsuario;
+//	private Logradouro logradouro;
+	private String login;
+	private String senha;
+//	private Permissoes permissoes;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public TipoUsuario getTipoUsuario() {
+		return tipoUsuario;
+	}
+	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+}
